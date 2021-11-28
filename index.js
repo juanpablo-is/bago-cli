cead7ddd008cb8e9a070959b425fbf3b8ccd0729
Juan Pablo Barrera <juanpablo.is@outlook.com>
2021-11-28 00:48:57 -0500

    Basic functionality to create folder

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 
 const { sayQuestions } = require('./lib/questions');
+const CreateStructure = require('./lib/createStructure');
 
 const run = async () => {
   const answers = await sayQuestions();
+  const structure = new CreateStructure(answers);
+  structure.createStructure();
 };
 
 run();
