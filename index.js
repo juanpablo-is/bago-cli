642a10e62539ebe4c21c0a15a89ed707cbe3bf2e
Juan Pablo Barrera <juanpablo.is@outlook.com>
2021-11-28 20:12:09 -0500

    Add questions about git and methods to execute commands git.

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 
 const { sayQuestions } = require('./lib/questions');
-const CreateStructure = require('./lib/createStructure');
+const { CreateStructure } = require('./lib/createStructure');
 
 const run = async () => {
   const answers = await sayQuestions();
+
   const structure = new CreateStructure(answers);
-  structure.createStructure();
+  await structure.createStructure();
 };
 
-run();
+run().then(null);
