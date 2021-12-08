b2bda35b6203e6c4c51522c097679f39347e675a
Juan Pablo Barrera <juanpablo.is@outlook.com>
2021-12-08 00:27:11 -0500

    Fix commits deleted for leveling repositories.

---
+++ README.md
@@ -0,0 +1,3 @@
+- Crear proyecto estructurado en nodejs solo backend (https://fazlulkabir34.medium.com/how-i-structured-my-first-express-app-5f9dd73370d4).
+- Crear proyecto estructurado en nodejs con renderizado. (templates engine)
+
--- index.js
+++ index.js
@@ -1,13 +1,60 @@
 #!/usr/bin/env node
 
-const { sayQuestions } = require('./lib/questions');
+const inquirer = require('inquirer');
+const { Subject } = require('rxjs');
+
 const { CreateStructure } = require('./lib/createStructure');
+const { questions } = require('./lib/questions');
 
-const run = async () => {
-  const answers = await sayQuestions();
+class Questions {
+  config = {};
+  prompts = new Subject();
 
-  const structure = new CreateStructure(answers);
-  await structure.createStructure();
-};
+  execute() {
+    inquirer.prompt(this.prompts).ui.process.subscribe({
+      next: (data) => {
+        const { name, answer } = data;
+        this.config[name] = answer;
 
-run().then(null);
+        let item = questions.shift();
+        if (!item) {
+          return this.prompts.complete();
+        }
+
+        // Func 'complete' to validate and determinate if the CLI complete.
+        if (item.complete) {
+          const complete = item.complete(this.config);
+
+          if (complete) {
+            return this.prompts.complete();
+          }
+        }
+
+        // Execute func 'when' to prevent stop CLI.
+        if (item.when && !item.when(this.config)) {
+          item = questions.shift();
+
+          if (!item) {
+            return this.prompts.complete();
+          }
+        }
+
+        this.prompts.next(item);
+      },
+      error: null,
+      complete: async () => {
+        try {
+          const structure = new CreateStructure(this.config);
+          await structure.createStructure();
+        } catch (e) {
+          inquirer.prompt(this.prompts).ui.process.unsubscribe();
+        }
+      },
+    });
+
+    // Process the first questions to subscribe to prompt.
+    this.prompts.next(questions.shift());
+  }
+}
+
+new Questions().execute();
